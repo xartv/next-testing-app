69163c9146f02192cd8bef1a06fe77f5a26292e3
Artem Vasilev <xartvdev@gmail.com>
2023-12-13 00:04:51 +0400

    test env, headers, cookies

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,13 +1,19 @@
 import { NextResponse } from "next/server";
+import { headers, cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import { posts } from "../posts";
 
 export async function GET(
   req: Request,
   { params }: { params: { id: string } }
 ) {
+  const secretInfo = process.env.SECRET;
   const id = params.id;
+  const userAgentHeader = headers().get("User-Agent");
+
+  const cookie = cookies().get("Cookie_2")?.value;
 
   const post = posts.find((post) => post.id === Number(id));
 
-  return NextResponse.json({ post });
+  return NextResponse.json({ post, userAgentHeader, cookie, secretInfo });
 }
