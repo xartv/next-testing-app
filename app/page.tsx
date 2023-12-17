cc587b8abb49207fc5835953da7fe8bc6b307040
Artem Vasilev <xartvdev@gmail.com>
2023-12-17 21:09:56 +0400

    test Next-Image

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import NextImage from "next/image";
+import Cat from "@/assets/cat.webp";
+
 export default function Home() {
-  return <h1>HOME</h1>;
+  return (
+    <>
+      <h1>HOME</h1>
+      <NextImage src={Cat} alt="cat" priority />
+    </>
+  );
 }
